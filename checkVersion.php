4ef8f50b9d97921251aeead1886826c379f36c5b
Arie290698 <33876123+Arie290698@users.noreply.github.com>
2020-09-14 00:26:18 +0700

    Update checkVersion.php

--- checkVersion.php
+++ checkVersion.php
@@ -1,3 +1,4 @@
 { 
-  "build": "1"
+  "build": "1",
+  "keywords": "'game', 'gaming', 'video', 'anime', 'wallpaper', 'image', 'photo', 'manga', 'comic', 'social media', 'facebook', 'hosting', 'computer', 'pc', 'smartphone', 'android', 'ios', 'os', 'software', 'education', 'chat', 'bussiness', 'school', 'food', 'shop', 'internet', 'television', 'tv', 'isp', 'provider', 'sim card', 'fashion', 'book', 'news', 'entertainment', 'life style', 'date', 'dating', 'utility', 'invest', 'agency', 'insurance', 'lawyer', 'donation', 'fundraising', 'conference',  'credit', 'technology', 'fitness', 'class', 'trading'"
 }
